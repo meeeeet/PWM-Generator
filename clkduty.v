4ead4223026818017b96844f48160bdf7a12ddee
Meet Sangani <76646671+meeeeet@users.noreply.github.com>
2023-02-24 21:14:18 +0530

    Update clkduty.v

--- clkduty.v
+++ clkduty.v
@@ -9,35 +9,35 @@ module clkduty(
 );
 
 
-reg [7:0]count;
+reg [7:0]count=8'd0;
 reg [7:0]duty;
 assign clk= (count<duty)? 1:0;
 
-assign d=duty; 
+assign d=count; 
 
 always @(negedge clkin or negedge reset) begin
     if(reset==0) begin
-        count=8'd0;
-        duty=8'd0;
+        count<=8'd0;
     end
 
     else if (count== 8'd49) begin
-        count=8'd0;
+        count<=8'd0;
     end
 
     else
-        count=count+1'd1;
+        count<=count+1'd1;
 end
 
 
-always @(negedge inc or negedge dec) begin
-
-    if (inc==0) begin
-        duty=duty+8'd5;
+always @(negedge inc or negedge dec or negedge reset) begin
+    if (reset==1'b0) duty<=8'd0;
+	 
+    if (inc==1'b0) begin
+        duty <= duty+8'd5;
     end
 
-    if(dec==0)begin
-        duty=duty-8'd5;
+    if(dec==1'b0)begin
+        duty<=duty-8'd5;
     end
 end
 
